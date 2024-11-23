f12584c79baddc69ec1c1f36ae5cc4bf657d99de
tinarao <golovindanila1613@gmail.com>
2024-11-24 02:51:50 +0300

    added some protections to plow function

--- World/world_map.gd
+++ World/world_map.gd
@@ -3,6 +3,27 @@ extends TileMap
 const GROUND_LAYER = 0
 const GRASS_LAYER = 1
 
+var avaliable_to_crop_tiles_atlas: Array = [
+	Vector2i(1, 1),
+	Vector2i(10, 4),
+
+	Vector2i(0, 5),
+	Vector2i(1, 5),
+	Vector2i(2, 5),
+	Vector2i(3, 5),
+	Vector2i(4, 5),
+	Vector2i(5, 5),
+
+	Vector2i(0, 6),
+	Vector2i(1, 6),
+	Vector2i(2, 6),
+	Vector2i(3, 6),
+	Vector2i(4, 6),
+	Vector2i(5, 6),
+]
+
+# (0,5) -> (5,5)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -23,19 +44,24 @@ func plow_a_tile():
 
 	var mouse_pos = get_local_mouse_position()
 	var tile_coordinates = local_to_map(mouse_pos)
-
-
 	var is_tile_exists: bool = false
 
-	for layer in get_layers_count() - 1:
+	var goal_layer: int = 0
+	for layer in get_layers_count() - 3:
 		var tile_data = get_cell_source_id(layer, tile_coordinates)
 		if tile_data == -1:
 			continue
 
 		is_tile_exists = true
+		goal_layer = layer
 
 	if not is_tile_exists:
-		print("No tile!")
+		print("no tile")
+		return
+
+	var tile_atlas_coordinates = get_cell_atlas_coords(goal_layer, tile_coordinates)
+	if not avaliable_to_crop_tiles_atlas.has(tile_atlas_coordinates):
+		print("can't plow this tile")
 		return
 
 	var crop_atlas_pos: Vector2i = Vector2i(2, 6)
